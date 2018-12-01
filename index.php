065413ba42c869ac39e92129a24a11718ffdea8d
Oscarfabian <oscarfabianparra@gmail.com>
2018-12-01 09:57:05 -0300

    integracion proyecto renacimiento

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/estilo.css" type="text/css">
+</head>
+<body>
+<div id="contenedor">
+<h3> Esta es una seccion de desarrollo</h3>
+<h4> verás mucho información </h5>
+<h5> te va a servir</h5>
+
+<p>
+
+La torre Eiffel3​ (tour Eiffel, en francés), inicialmente llamada la tour de 300 mètres (torre de 300 metros), es una estructura de hierro pudelado diseñada por los ingenieros Maurice Koechlin y Émile Nouguier, dotada de su aspecto definitivo por el arquitecto Stephen Sauvestre y construida por el ingeniero francés <em>Alexandre Gustave Eiffel</em> y sus colaboradores para la Exposición Universal de 1889 en París
+
+
+</p>
+
+</div>
+<p>
+
+Situada en el extremo del  Campo de Marte  a la orilla del<em> río Sena</em>, este monumento parisino, símbolo de Francia y de su capital, es la estructura más alta de la ciudad y el monumento que cobra entrada más visitado del mundo, con 7,1 millones de turistas cada año.5​ Con una altura de 300 metros, prolongada más tarde con una antena hasta los 324 metros, la torre Eiffel fue la estructura más elevada del mundo durante 41 años
+
+</p>
+
+
+
+
+<input type="button" value="mas informacion">
+<br>
+
+<a href="https://fayerwayer.com" target="_blank"> Ir a fayerwayer </a>
+<br>
+<a href="https://cnnchile.com" target="_blank"> Ir a cnn chile </a>
+<br>
+<a href="https://visitandoeuropa.com" target="_blank""> Ir a visitandoeuropa </a>
+<br>
+<a href="https://lingohut.com" target="_blank"> Ir a lingohut </a>
+
+
+</body>
+</html>
